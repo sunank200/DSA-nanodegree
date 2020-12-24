bc1e9c81b11f92b002a3887d3249840d712f78dd
Ankit Chaurasia <sunank200@gmail.com>
2020-12-24 22:20:22 +0545

    Task1: The script correctly prints number of distinct telephone numbers in the dataset.

--- P0/Task1.py
+++ P0/Task1.py
@@ -19,3 +19,23 @@ How many different telephone numbers are there in the records?
 Print a message:
 "There are <count> different telephone numbers in the records."
 """
+
+
+def differentPhoneNumbers():
+    set_of_phone_numbers = set()
+    for each_text in texts:
+        set_of_phone_numbers.add(each_text[0])
+        set_of_phone_numbers.add(each_text[1])
+
+    for each_call in calls:
+        set_of_phone_numbers.add(each_call[0])
+        set_of_phone_numbers.add(each_call[1])
+
+    print(
+        "There are {} different telephone numbers in the records.".format(
+            len(set_of_phone_numbers)
+        )
+    )
+
+
+differentPhoneNumbers()
