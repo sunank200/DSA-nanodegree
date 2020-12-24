1fff79f0bf537e7ac2073eae39e757fb806c2a12
Ankit Chaurasia <sunank200@gmail.com>
2020-12-24 22:21:11 +0545

    Task 2 - The script correctly prints the telephone number that spent the longest time on the phone and the total time in seconds they spend on phone call.

--- P0/Task2.py
+++ P0/Task2.py
@@ -19,3 +19,33 @@ also time spent on the phone.
 Print a message:
 "<telephone number> spent the longest time, <total time> seconds, on the phone during September 2016.".
 """
+
+
+def longestCall():
+    call_dicts = {}
+    for each_call in calls:
+        if each_call[0] not in call_dicts:
+            call_dicts[each_call[0]] = int(each_call[3])
+        else:
+            call_dicts[each_call[0]] += int(each_call[3])
+
+        if each_call[1] not in call_dicts:
+            call_dicts[each_call[1]] = int(each_call[3])
+        else:
+            call_dicts[each_call[1]] += int(each_call[3])
+
+    longest_call = 0
+    longest_calling_number = None
+    for telephone_number in call_dicts.keys():
+        if call_dicts[telephone_number] > longest_call:
+            longest_call = call_dicts[telephone_number]
+            longest_calling_number = telephone_number
+
+    print(
+        "{} spent the longest time, {} seconds, on the phone during September 2016.".format(
+            longest_calling_number, longest_call
+        )
+    )
+
+
+longestCall()
